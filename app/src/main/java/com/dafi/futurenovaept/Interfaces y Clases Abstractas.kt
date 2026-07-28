75bc963993cef047f83139f006d24477c6c2d978
Dafne <nosexd.cl@gmail.com>
2026-07-27 23:50:20 -0500

    Interfaces y Clases Abstractas V1

---
+++ app/src/main/java/com/dafi/futurenovaept/Interfaces y Clases Abstractas.kt
@@ -0,0 +1,46 @@
+package com.dafi.futurenovaept
+
+fun main(){
+    val logger = ConsoleLogger()
+    logger.log("Mensaje de registro")
+    logger.error("Error ocurrido")
+
+    val userSyncWorker = UserSyncWorker()
+    userSyncWorker.sync()
+}
+
+interface Logger {
+    fun log(message: String)
+    fun error(message: String) = log("Error: $message")
+}
+
+class ConsoleLogger : Logger {
+    override fun log(message: String) {
+        println(message)
+    }
+
+}
+
+abstract class BaseSyncWorker {
+    fun sync(){
+        connect()
+        fetchdata()
+    }
+    private fun connect(){
+        println("Connecting...")
+    }
+    protected abstract fun fetchdata()
+
+}
+
+class UserSyncWorker : BaseSyncWorker(){
+    override fun fetchdata() {
+        println("Fetching user data...")
+    }
+}
+
+class ProductSyncWorker : BaseSyncWorker(){
+    override fun fetchdata() {
+        println("Fetching product data...")
+    }
+}
